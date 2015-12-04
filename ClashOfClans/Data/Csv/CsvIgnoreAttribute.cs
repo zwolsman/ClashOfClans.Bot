8cbac3505c5989e0de648621a243d2ccbcc0ddb1
Marvin <marvin.zwolsman@gmail.com>
2015-12-04 19:11:42 +0100

    Added CSV's and decompressing/reading

---
+++ ClashOfClans/Data/Csv/CsvIgnoreAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClashOfClans.Data.Csv
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class CsvIgnoreAttribute : Attribute
+    {
+        public CsvIgnoreAttribute()
+        {
+            
+        }
+    }
+}
